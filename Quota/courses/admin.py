5a9093efd7852b380d2f64f2397ddc66193c1c4b
6410685033 <6410685033@student.tu.ac.th>
2022-09-16 00:10:03 +0700

    startapp users

--- Quota/courses/admin.py
+++ Quota/courses/admin.py
@@ -3,11 +3,5 @@ from .models import Course, Student
 
 # Register your models here.
 
-class CourseAdmin(admin.ModelAdmin):
-    list_display = ("id", "code", "name")
-
-class StudentAdmin(admin.ModelAdmin):
-    filter_horizontal = ("Courses",)
-
 admin.site.register(Course)
 admin.site.register(Student)
--- Quota/courses/views.py
+++ Quota/courses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Course, Student
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 def index(request):
@@ -14,3 +15,11 @@ def course(request, course_code):
     return render(request, "courses/course.html", {
         "course": course,
     })
+
+def enroll(request, course_code):
+    if request.method == "POST":
+        course = Course.objects.get(code=course_code)
+        student = Student.objects.get(code=str(request.POST["student"]))
+        student.courses.add(course)
+        # flight.passengers.add(passenger)
+        return HttpResponseRedirect(reverse("course", args=(course.code,)))
