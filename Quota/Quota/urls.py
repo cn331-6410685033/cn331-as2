92dc36cb70e650d93f808d0a34259801b0f1d0f5
6410685033 <88651132+6410685033@users.noreply.github.com>
2022-09-16 00:16:03 +0700

    Revert "Quota"

--- Quota/Quota/urls.py
+++ Quota/Quota/urls.py
@@ -19,6 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('courses/', include('courses.urls')),
-    path('users/', include('users.urls')),
-    path('', include('users.urls')),
 ]
