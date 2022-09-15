749769fd57b94b09d1e4d6f3814fed7642d8b69c
6410685033 <6410685033@student.tu.ac.th>
2022-09-16 00:07:35 +0700

    reset

--- Quota/courses/admin.py
+++ Quota/courses/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course, Student
 
 # Register your models here.
 
 class CourseAdmin(admin.ModelAdmin):
     list_display = ("id", "code", "name")
 
+class StudentAdmin(admin.ModelAdmin):
+    filter_horizontal = ("Courses",)
+
 admin.site.register(Course)
+admin.site.register(Student)
--- Quota/courses/views.py
+++ Quota/courses/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from .models import Course
+from .models import Course, Student
 
 # Create your views here.
 
 def index(request):
     return render(request, "courses/index.html", {
-        "courses": Course.objects.all()
+        "courses": Course.objects.all(),
+        "students": Student.objects.all(),
     })
 
 def course(request, course_code):
