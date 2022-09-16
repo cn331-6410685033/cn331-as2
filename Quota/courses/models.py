1e32d120cc0253718263f98bcc71f64db862fb8f
6410685033 <6410685033@student.tu.ac.th>
2022-09-16 12:03:05 +0700

    add class Student

--- Quota/courses/models.py
+++ Quota/courses/models.py
@@ -1,3 +1,4 @@
+from operator import mod
 from django.db import models
 
 # Create your models here.
@@ -9,3 +10,8 @@ class Course(models.Model):
 
     def __str__(self):
         return f"{self.code}: {self.name}"
+
+class Student(models.Model):
+    first = models.CharField(max_length=64)
+    last = models.CharField(max_length=64)
+    courses = models.ManyToManyField(Course, blank=True, related_name="courses")
