bf46fb9a13395fdf7255cb070eac9f338d6a8603
6410685033 <6410685033@student.tu.ac.th>
2022-10-08 14:22:31 +0700

    add test models

--- Quota/courses/tests.py
+++ Quota/courses/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Course
 
 # Create your tests here.
+
+class CoursesTestCase(TestCase):
+
+    def setUp(self):
+        #create courses
+        course1 = Course.objects.create(code="CN101", name="Python", max_seat=3)
+
+    def test_seat_available(self):
+        # is_seat_available should be True
+        course = Course.objects.first()
+
+        self.assertTrue(course.is_seat_available())
+
+    def test_seat_not_available(self):
+        # is_seat_available should be False
+        student1 = User.objects.create_user(username="student1")
+        student2 = User.objects.create_user(username="student2")
+        student3 = User.objects.create_user(username="student3")
+
+        course = Course.objects.first()
+        course.students.add(student1)
+        course.students.add(student2)
+        course.students.add(student3)
+
+
+        self.assertTrue(course.is_seat_available())
+
+    def test_to_string(self):
+        course = Course.objects.get(code="CN101")
+
+        self.assertEqual(str(course),"CN101: Python")
